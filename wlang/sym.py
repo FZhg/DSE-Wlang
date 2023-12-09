69d7e64a0578c6710c4723631515565b9cac8c80
fzhg <m42zhang@uwaterloo.ca>
2023-12-09 11:08:49 -0500

    finished assign statement

--- wlang/sym.py
+++ wlang/sym.py
@@ -24,6 +24,8 @@ import io
 import sys
 import logging
 import z3
+
+from wlang.int import State, Interpreter
 from . import ast, int, undef_visitor
 
 
@@ -109,25 +111,336 @@ class SymState(object):
 
 
 # TODO: finish skeletons
-class ConcreteState:
+class ConcreteState(State):
     def __init__(self):
-        pass
+        super().__init__()
+
+    def update_variables(self, variable_name: str, value: int):
+        self.env[variable_name] = value
 
 
 # TODO: finish skeletons
 class ProgramState:
     def __init__(self):
-        self.symState = SymState()
-        self.concreteState = ConcreteState()
+        self.sym_state = SymState()
+        self.concrete_state = ConcreteState()
+
+    def update_variable(self, variable_name: str, sym_expression, concrete_value: int):
+        self.sym_state.update_variable(variable_name, sym_expression)
+        self.concrete_state.update_variables(variable_name, concrete_value)
+
+    def get_sym_state(self):
+        return self.sym_state
+
+    def get_concrete_state(self):
+        return self.concrete_state
+
+    def is_error(self):
+        return self.sym_state.is_error()
+
+    def is_infeasible(self):
+        return self.sym_state.is_empty()
 
 
-class EXEDynamicSymExec(ast.AstVisitor):
+class DynamicSysExec(ast.AstVisitor):
     def __init__(self):
         super().__init__()
 
+    # No need to visit expressions and atoms
+
+    def visit_Stmt(self, node, *args, **kwargs):
+        return kwargs['states']  # PrintState and Skip
+
+    def _get_new_sym__state_kwarg(self, sym_state: SymState):
+        """
+        used for the sym visitor to visit expressions
+        """
+        return {
+            'state': sym_state
+        }
+
+    def _get_new_concrete_state_kwarg(self, concrete_state: ConcreteState):
+        return {
+            'state': concrete_state
+        }
+
+    def visit_AsgnStmt(self, node, *args, **kwargs):
+        states = kwargs["states"]
+        for index, state in enumerate(states):
+            if state.is_error() or state.is_infeasible():
+                continue
+            # TODO: sym visitor + concrete visitor
+            # TODO: modifies the update_variable method to take both sym and con states
+            # sym exec
+            sym_state = state.get_sym_state()
+            new_sym_state_kwargs = self._get_new_sym__state_kwarg(sym_state)
+            sym_visitor = SymExec()
+            rhs_sym_expr = sym_visitor.visit(node.rhs, **new_sym_state_kwargs)
+
+            # concrete exec
+            concrete_state = state.get_concrete_state()
+            new_concrete_state_kwargs = self._get_new_concrete_state_kwarg(concrete_state)
+            concrete_visitor = Interpreter()
+            rhs_concrete_value = concrete_visitor.visit(node.rhs, **new_concrete_state_kwargs)
+
+            state.update_variable(node.lhs.name, rhs_sym_expr, rhs_concrete_value)
+            states[index] = state
+        return states
+
+    def visit_IfStmt(self, node, *args, **kwargs):
+        pass
 
 
 
+    def visit_WhileStmt(self, node, *args, **kwargs):
+        pass
+
+
+class SymExec(ast.AstVisitor):
+    def __init__(self):
+        super().__init__()
+
+    def run(self, abstract_syntax_tree, state):
+        # set things up and
+        # call self.visit (ast, states=states)
+        states = [state]
+        states = self.visit(abstract_syntax_tree, states=states)
+        normal_states = []
+        for state in states:
+            if state.is_error():
+                logging.error("Error State: " + str(state))
+            elif state.is_empty():
+                logging.info("Infeasible State: " + str(state))
+            else:
+                normal_states.append(state)
+        return normal_states
+
+    def visit_IntVar(self, node, *args, **kwargs):
+        return kwargs['state'].env[node.name]
+
+    def visit_BoolConst(self, node, *args, **kwargs):
+        return z3.BoolVal(node.val)
+
+    def visit_IntConst(self, node, *args, **kwargs):
+        return z3.IntVal(node.val)
+
+    def visit_RelExp(self, node, *args, **kwargs):
+        lhs = self.visit(node.arg(0), *args, **kwargs)
+        rhs = self.visit(node.arg(1), *args, **kwargs)
+        if node.op == "<=":
+            return lhs <= rhs
+        if node.op == "<":
+            return lhs < rhs
+        if node.op == "=":
+            return lhs == rhs
+        if node.op == ">=":
+            return lhs >= rhs
+        if node.op == ">":
+            return lhs > rhs
+        raise "Unknown Operators"
+
+    def visit_BExp(self, node, *args, **kwargs):
+        operands = [self.visit(operand_node, *args, **kwargs) for operand_node in node.args]
+        if node.is_unary():
+            assert len(operands) == 1
+            if node.op == "not":
+                return z3.Not(operands[0])
+        else:
+            assert len(operands) == 2
+            if node.op == "and":
+                return z3.And(*operands)
+            elif node.op == "or":
+                return z3.Or(*operands)
+        raise "Unknown Operators"
+
+    def visit_AExp(self, node, *args, **kwargs):
+        # only a single state is passed down
+        # expression will alter state
+        operands = [self.visit(operand_node, *args, **kwargs) for operand_node in node.args]
+        assert len(operands) == 2
+        if node.op == "+":
+            return operands[0] + operands[1]
+        elif node.op == "-":
+            return operands[0] - operands[1]
+        elif node.op == "*":
+            return operands[0] * operands[1]
+        elif node.op == "/":
+            return operands[0] / operands[1]
+        raise "Unknown Operators"
+
+    def visit_SkipStmt(self, node, *args, **kwargs):
+        return kwargs['states']
+
+    def visit_PrintStateStmt(self, node, *args, **kwargs):
+        return kwargs['states']
+
+    def visit_AsgnStmt(self, node, *args, **kwargs):
+        states = kwargs["states"]
+        for index, state in enumerate(states):
+            if state.is_error() or state.is_empty():
+                continue
+            new_kwargs = dict(kwargs)
+            new_kwargs['state'] = state
+            rhs = self.visit(node.rhs, *args, **new_kwargs)
+            state.update_variable(node.lhs.name, rhs)
+            states[index] = state
+        return states
+
+    def visit_IfStmt(self, node, *args, **kwargs):
+        # infeasible => else ; if else branch didn't exit, path condition remains the same
+        # feasible => then else branch; change the path conditions.
+
+        # feasible run the then branch
+        # infeasible => else;
+
+        states = kwargs["states"]
+        new_added_states = []
+        for index, state in enumerate(states):
+            if state.is_error() or state.is_empty():
+                continue
+            then_state, else_state = state.fork()
+            new_kwargs = dict(kwargs)
+            new_kwargs["state"] = state
+            condition = self.visit(node.cond, *args, **new_kwargs)
+            then_state.add_pc(condition)
+            if not then_state.is_empty():
+                # then branch
+                new_then_states = [then_state]
+                new_then_kwargs = dict(kwargs)
+                new_then_kwargs["states"] = new_then_states
+                new_then_states = self.visit(node.then_stmt, *args, **new_then_kwargs)
+                states[index] = new_then_states[0]  # TODO: this should be unnecessary
+                new_added_states.extend(new_then_states[1:])
+            else:
+                states[index] = then_state
+
+            # else branch
+            else_state.add_pc(z3.Not(condition))
+            if not else_state.is_empty():
+                if node.has_else():
+                    new_else_states = [else_state]
+                    new_else_kwargs = dict(kwargs)
+                    new_else_kwargs["states"] = new_else_states
+                    new_else_states = self.visit(node.else_stmt, *args, **new_else_kwargs)
+                    new_added_states.extend(new_else_states)
+                else:
+                    new_added_states.append(else_state)
+            else:
+                new_added_states.append(else_state)
+        states.extend(new_added_states)
+        return states
+
+    def verify_loop_inv(self, inv, cond, body, states):
+        node = SymExec._generated_loop_inv_verifying_code(inv, cond, body)
+        return self.visit(node, states=states)
+
+    @staticmethod
+    def _generated_loop_inv_verifying_code(inv, cond, body):
+        assert_stmt_1 = ast.AssertStmt(inv)
+        uv = undef_visitor.UndefVisitor()
+        uv.check(body)
+        variables_modified = list(uv.get_defs())
+        if len(variables_modified) == 0:
+            havoc_stmt = ast.SkipStmt()
+        else:
+            havoc_stmt = ast.HavocStmt(variables_modified)
+        assume_stmt_1 = ast.AssumeStmt(inv)
+        assert_stmt_2 = ast.AssertStmt(inv)
+        false_const = ast.BoolConst(False)
+        assume_stmt_2 = ast.AssumeStmt(false_const)
+        if_body = ast.StmtList([body, assert_stmt_2, assume_stmt_2])
+        if_stmt = ast.IfStmt(cond, if_body)
+        root_node = ast.StmtList([assert_stmt_1, havoc_stmt, assume_stmt_1, if_stmt])
+        # print_visitor = ast.PrintVisitor()
+        # print_visitor.visit(root_node)
+        return root_node
+
+    def visit_WhileStmt(self, node, *args, **kwargs):
+        states = kwargs["states"]
+        if node.has_inv():
+            states = self.verify_loop_inv(node.inv, node.cond, node.body, states)
+        else:
+            upstream_loop_unrolling_count = 0
+            if "loop_unrolling_count" in kwargs.keys():
+                upstream_loop_unrolling_count = kwargs["loop_unrolling_count"]
+            new_added_states = []
+            for index, state in enumerate(states):
+                if state.is_error() or state.is_empty():
+                    continue
+                new_kwargs = dict(kwargs)
+                new_kwargs["state"] = state
+                condition = self.visit(node.cond, *args, **new_kwargs)
+                state, exiting_state = state.fork()
+                exiting_state.add_pc(z3.Not(condition))
+                state.add_pc(condition)
+                if state.is_empty() or upstream_loop_unrolling_count >= 10:
+                    states[index] = exiting_state
+                    continue
+                if not exiting_state.is_empty():
+                    new_added_states.append(exiting_state)
+                new_states = [state]
+                new_kwargs = dict(kwargs)
+                new_kwargs["states"] = new_states
+                new_states = self.visit(node.body, *args, **new_kwargs)
+                new_kwargs["states"] = new_states
+                loop_unrolling_count = upstream_loop_unrolling_count + 1
+                new_kwargs["loop_unrolling_count"] = loop_unrolling_count
+                new_states = self.visit(node, *args, **new_kwargs)
+                states[index] = new_states[0]  # TODO: this should be unnecessary
+                new_added_states.extend(new_states[1:])
+            states.extend(new_added_states)
+        return states
+
+    def visit_AssertStmt(self, node, *args, **kwargs):
+        # Don't forget to print an error message if an assertion might be violated
+        states = kwargs['states']
+        for index, state in enumerate(states):
+            if state.is_error() or state.is_empty():
+                continue
+            new_kwargs = dict(kwargs)
+            new_kwargs["state"] = state
+            condition = self.visit(node.cond, *args, **new_kwargs)
+            _, counter_state = state.fork()
+            counter_condition = z3.Not(condition)
+            counter_state.add_pc(counter_condition)
+            if counter_state.is_empty():
+                state.add_pc(condition)  # TODO: this should be unnecessary
+            else:
+                state.mk_error()
+                logging.error("Assertion Invalid: " + str(node))
+            states[index] = state
+        return states
+
+    def visit_AssumeStmt(self, node, *args, **kwargs):
+        states = kwargs['states']
+        for index, state in enumerate(states):
+            if state.is_error() or state.is_empty():
+                continue
+            new_kwargs = dict(kwargs)
+            new_kwargs["state"] = state
+            condition = self.visit(node.cond, *args, **new_kwargs)
+            state.add_pc(condition)
+            if state.is_empty():
+                logging.info("Assumption unfeasible: " + str(node))
+            states[index] = state
+        return states
+
+    def visit_HavocStmt(self, node, *args, **kwargs):
+        states = kwargs["states"]
+        for state in states:
+            if state.is_error() or state.is_empty():
+                continue
+            for variable in node.vars:
+                state.add_new_variable(variable.name)
+        return states
+
+    def visit_StmtList(self, node, *args, **kwargs):
+        states = kwargs["states"]
+        new_kwargs = dict(kwargs)
+        for stmt in node.stmts:
+            new_kwargs["states"] = states
+            states = self.visit(stmt, *args, **new_kwargs)
+        return states
 
 
 def _parse_args():
@@ -144,7 +457,7 @@ def main():
     args = _parse_args()
     prg = ast.parse_file(args.in_file)
     st = ProgramState()
-    sym = EXEDynamicSymExec()
+    sym = SymExec()
 
     states = sym.run(prg, st)
     if states is None:
